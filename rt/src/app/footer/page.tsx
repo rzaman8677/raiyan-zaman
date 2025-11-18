1cda653a706bb39d5beb30825525ce44d2fb5ca8
Raiyan Zaman <77029110+rzaman8677@users.noreply.github.com>
2025-11-18 17:20:32 +0000

    Add correct link for instgram instead of tiktok

--- rt/src/app/footer/page.tsx
+++ rt/src/app/footer/page.tsx
@@ -1,9 +1,9 @@
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons/faLinkedin";
-import { faTiktok } from "@fortawesome/free-brands-svg-icons/faTiktok";
 import { faYoutube } from "@fortawesome/free-brands-svg-icons/faYoutube";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./Footer.module.css"; 
 import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => (
     <div className={styles.footer}>
@@ -17,12 +17,12 @@ const Footer = () => (
           <FontAwesomeIcon icon={faYoutube} />
         </a>
         <a
-          href="https://www.tiktok.com/@isldw"
+          href="https://www.instagram.com/raiyanzaman30/"
           target="_blank"
           rel="noopener noreferrer"
           className={styles.socialIcon}
         >
-          <FontAwesomeIcon icon={faTiktok} />
+          <FontAwesomeIcon icon={faInstagram} />
         </a>
         <a
           href="https://www.linkedin.com/in/raiyan-zaman-a94653269"
